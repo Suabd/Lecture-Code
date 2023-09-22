8cd8dffd46d4033e6690d88819ab20dacffcbd7d
Your Name <you@example.com>
2023-09-22 08:54:10 -0500

    class codes

--- src/main/java/week3/CorporateSponsors.java
+++ src/main/java/week3/CorporateSponsors.java
@@ -1,11 +1,19 @@
 package week3;
 
+import java.util.Arrays;
+
 public class CorporateSponsors {
     public static void main(String[] args) {
 
         // modify array make strings uppercase
-        String[] sponsore = { "ikea", "at&t", "cvs", "3m"};
+        String[] sponsors = { "ikea", "at&t", "cvs", "3m"};
 
-        for (int i = 0; i < sponsore.length; i++);
+        for (int i = 0; i < sponsors.length; i++) {
+            String sponsor = sponsors[i];
+            sponsor = sponsor.toUpperCase();
+            sponsors[i] = sponsor;
+        }
+
+        System.out.println(Arrays.toString(sponsors));
     }
 }
